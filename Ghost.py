14ea3f2ce573245be73224e4fbbd07f79ae5a3d3
smalyala <sahith.malyala325@gmail.com>
2014-12-10 18:17:10 -0500

    import working

--- Ghost.py
+++ Ghost.py
@@ -1,9 +1,13 @@
+from random import choice
+import Trie
+
 def printGhostDirections():
 	print(' +------------------------------------+');
 	print(' |    Welcome to the game of GHOST.   |');
 	print(' |    The human goes first. Enter     |');
 	print(' |    your letters in the pop-up      |');
 	print(' |    dialog boxes. Good luck.        |');
+	print(' +------------------------------------+');
 
 def requestAndCheckHumanMove(root, stng):
 	stng += input('HUMAN, enter your character: ').lower()[0];
@@ -11,7 +15,7 @@ def requestAndCheckHumanMove(root, stng):
 	if root.search(stng) == True:
 		print('------------------------------------------');
 		print(' HUMAN LOSES because "', stng, '" is a word.', sep = '');
-		print('---------------< GAME OVER >--------------'):
+		print('---------------< GAME OVER >--------------');
 		exit();
 	if root.fragmentInDictinoary(stng) == False:
 		print('------------------------------------------');
@@ -19,12 +23,24 @@ def requestAndCheckHumanMove(root, stng):
 			  '"\n does not begin any word.', sep = '');
 		print(" [The computer's word was ", '"', \
 			    spellWordFromString(root, stng[0:-1]), '".]', sep = '');
-		print('---------------< GAME OVER >--------------'):
+		print('---------------< GAME OVER >--------------');
 		exit();
 	return stng;
 
+def createTrieFromDictionaryFile():
+	file1 = open('GhostDictionary.py');
+	root = Trie.Node('*');
+	for word in file1:
+		root.insert(word.lower().strip());
+	file1.close();
+	return root;
+
+def randomChild(self):
+	children = self.children;
+
 def main():
 	root = createTrieFromDictionaryFile();
+	root.display();
 	printGhostDirections();
 	stng = '';
 	while True:
